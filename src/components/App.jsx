4d322dd6fb38f0c4642215478a560f46426d4830
IlyaSV99 <ilavelicko81@gmail.com>
2023-01-07 14:09:05 +0200

    added localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,53 +7,59 @@ import ContactList from 'components/ContactList';
 import { nanoid } from 'nanoid';
 
 export class App extends Component {
-
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contactsList');
+    const parsedContacts = JSON.parse(contacts);
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contactsList', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handelFormSubmit = data => {
     const { name } = data;
     const { contacts } = this.state;
     const sameName = contacts.some(el => el.name === name);
-    
-    sameName ? alert(`${name} is already in contacts.`) : this.setState(prevState => ({
-      contacts: [...prevState.contacts, { id: nanoid(), ...data }],
-    }));
+
+    sameName
+      ? alert(`${name} is already in contacts.`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, { id: nanoid(), ...data }],
+        }));
   };
   handleFilterData = e => {
     const { value } = e.target;
     this.setState({
       filter: value,
-    })
-    }
+    });
+  };
   getFilteredContacts() {
     const { filter, contacts } = this.state;
-    
+
     if (!filter) {
-      return contacts
+      return contacts;
     }
-    return contacts.filter(({name}) =>
+    return contacts.filter(({ name }) =>
       name.toLowerCase().includes(filter.toLowerCase())
     );
   }
-  removeContact = id=> {
+  removeContact = id => {
     this.setState(prevState => ({
-      contacts: prevState.contacts.filter(el => el.id !== id),  
+      contacts: prevState.contacts.filter(el => el.id !== id),
     }));
-  }
+  };
 
   render() {
-    
     const { handelFormSubmit, handleFilterData, removeContact } = this;
     const filtredContacts = this.getFilteredContacts();
-
 
     return (
       <>
@@ -61,11 +67,8 @@ export class App extends Component {
           <ContactForm onSubmit={handelFormSubmit} />
         </Section>
         <Section title="Contacts">
-          <Filter
-            title="Find contacts by name"
-            filter={handleFilterData}
-          />
-          <ContactList contacts={filtredContacts} onClick={removeContact} />          
+          <Filter title="Find contacts by name" filter={handleFilterData} />
+          <ContactList contacts={filtredContacts} onClick={removeContact} />
         </Section>
       </>
     );
